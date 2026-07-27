414db771c5a409f5e8484ebf23b64ef23fcbfd41
Amawethu-Anathi Mihle Gladile <amawethugladile@gmail.com>
2026-07-27 12:27:07 +0200

    Concert ticket booker program

--- Phase_1/Lesson_1.py
+++ Phase_1/Lesson_1.py
@@ -1,4 +1,4 @@
-#Concert ticket booker program
+#Concert ticket booker program 
 name = input("Enter your name: ")
 artist = input("Enter the artist you want to see: ")
 print(f"Hey {name}! Your ticket to see {artist} is booked successfully!")
