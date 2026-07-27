a4e23f2eb45c13884c194cd4fe5cd08fa3cdbac0
Amawethu-Anathi Mihle Gladile <amawethugladile@gmail.com>
2026-07-27 17:54:26 +0200

    The Phone Directory Search

---
+++ Phase_1/Lesson_6.py
@@ -0,0 +1,11 @@
+#The Phone Directory Search
+
+contacts = {"emihle" : "083 123 4567", "sharon": "082 987 6543", "nashe": "081 234 5678"}
+
+name_to_search = input("Enter the name of the contact you want to search for: ").lower()
+for name in contacts:
+    if name == name_to_search:
+        print(f"Found! {name_to_search}'s number is  {contacts[name_to_search]}")
+        break
+else:
+    print("Contact not found.")
